1a8e02d51d020e3901fed90a528dfc070e95a482
hauntedghoul <madison.mitchell6704@gmail.com>
2024-05-16 17:37:07 -0600

    background image
    added the background image via link, cant figure out how to do the rest

--- botanicaretaker/src/componets/pages/home.js
+++ botanicaretaker/src/componets/pages/home.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import './home.css';
+
 
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>Welcome to the Home Page!</p>
+    <div className='home'>
+      {/* <h1>Home Page</h1>
+      <p>Welcome to the Home Page!</p> */}
+      <div className='plant'>
+        <img src='botanicaretaker\public\images\plant7.PNG' />
+      </div>
+
     </div>
   );
 }
