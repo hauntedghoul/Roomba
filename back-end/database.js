6711e755ecb6c9ed1925c2b8a63973bb88988ae2
Khureh47 <123666159+Khureh47@users.noreply.github.com>
2024-05-16 19:39:12 -0600

    Idk

--- back-end/database.js
+++ back-end/database.js
@@ -3,16 +3,25 @@ const { MongoClient } = require("mongodb");
 const uri = "mongodb+srv://roomba:Roomba01@cluster0.fm4mkz2.mongodb.net/";
 const client = new MongoClient(uri);
 
-const database = client.db('Roomba');
-const users = database.collection("User")
-const logs = database.collection("Logs")
-
 function addUser(user, pass) {
-    
+    try {
+        const database = client.db('Roomba');
+        const users = database.collection("User");
+
+        const doc = {
+            Username: user,
+            Password: pass
+        }
+
+        const result = users.insertOne(doc);
+        console.log('Inserted');
+    } finally {
+        client.close();
+    }
 }
 
 function viewAllLog() {
-
+    
 }
 
 function openLog() {
