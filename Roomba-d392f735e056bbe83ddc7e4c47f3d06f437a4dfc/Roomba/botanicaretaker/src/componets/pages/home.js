57076d402536808cac97c945537c2b59818b2fa9
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2024-05-20 12:17:56 -0600

    Add files via upload

---
+++ Roomba-d392f735e056bbe83ddc7e4c47f3d06f437a4dfc/Roomba/botanicaretaker/src/componets/pages/home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './home.css';
+
+function Home() {
+  return (
+    <div className='home'>
+      <div>
+        {/* <h1>Home Page</h1>
+      <p>Welcome to the Home Page!</p> */}
+        <div className='plant'>
+        <img src='/images/watering.PNG' alt='watering' />
+        <button>
+          <img className='can' src='/images/plant7.PNG' alt='Plant' /> 
+        </button>
+<h1>does this work?
+  
+</h1>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default Home;
