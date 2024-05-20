dcc293f8df4e1096e1016324d14996920878f047
YellowYoCat <125392679+YellowYoCat@users.noreply.github.com>
2024-05-20 12:21:05 -0600

    Add files via upload

---
+++ Roomba/botanicaretaker/src/componets/pages/seedpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SeedPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Seed Page</h1>
+      <img src='/images/Underconstruction.PNG' alt="Example" />
+    </div>
+  );
+};
+
+export default SeedPage;
