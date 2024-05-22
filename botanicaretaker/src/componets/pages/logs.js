e5ccbb803847c0a94843303d7ed64c2904ebe912
Khureh47 <123666159+Khureh47@users.noreply.github.com>
2024-05-21 18:16:20 -0600

    Update logs.js

--- botanicaretaker/src/componets/pages/logs.js
+++ botanicaretaker/src/componets/pages/logs.js
@@ -1,34 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './logs.css'
 
 function Logs() {
+
+  const [logs, setLogs] = useState([
+    {
+      Stage: 'Testings',
+      Height: '7m',
+      Health: 'Dead',
+      Time: '2024-05-17T21:26:41.336+00:00'
+    },
+    {
+      Stage: 'Testings',
+      Height: '7m',
+      Health: 'Dead',
+      Time: '2024-05-17T21:26:41.336+00:00'
+    }])
+
   return (
-    <div className="container">
-      <h1>Logs</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>User</th>
-            <th>Action</th>
+  <div className="container">
+    <h1>Logs</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Stage</th>
+          <th>Height</th>
+          <th>Health</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* <tr>
+          <th>Testings</th>
+          <th>7m</th>
+          <th>Dead</th>
+          <th>2024-05-17</th>
+        </tr> */}
+        {logs.map((log, index) => (
+          <tr key={index}>
+            <td>{log.Stage}</td>
+            <td>{log.Height}</td>
+            <td>{log.Health}</td>
+            <td>{log.Time}</td>
           </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>2023-03-15</td>
-            <td>John Doe</td>
-            <td>Created new user</td>
-          </tr>
-          <tr>
-            <td>2023-03-14</td>
-            <td>Jane Doe</td>
-            <td>Deleted log entry</td>
-          </tr>
-        </tbody>
-      </table>
-      <button>Clear Logs</button>
-    </div>
-  );
-}
+        ))}
+      </tbody>
+    </table>
+    <button>Clear Logs</button>
+  </div>
+)};
+
 
 export default Logs;
