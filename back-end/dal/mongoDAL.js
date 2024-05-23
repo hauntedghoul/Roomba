0632091be010d9280d3acd4c8349572ce99f4725
Khureh47 <123666159+Khureh47@users.noreply.github.com>
2024-05-22 18:29:02 -0600

    push

--- back-end/dal/mongoDAL.js
+++ back-end/dal/mongoDAL.js
@@ -40,5 +40,28 @@ mongoose.connect('mongodb+srv://mmitchell:Tuff12top@cluster0.fm4mkz2.mongodb.net
     .catch(err => console.error(err));
 
 exports.DAL = {
+
+    createPlant: async (plantData) => {
+        const plant = new Plant(plantData);
+        return await plant.save();
+    },
+
+    getPlantById: async (plantId) => {
+        return await Plant.findById(plantId).populate('logID').exec();
+    },
+
+    createLog: async (logData) => {
+        const log = new Log(logData);
+        const savedLog = await log.save();
+        await Plant.findByIdAndUpdate(logData.plantId, {$push: { logID: savedLog._id} });
+    },
+
+    getLogsByPlantId: async (plantId) => {
+        return await Log.find({ plantId }).exec();
+    },
+
+    getAllLogs: async () => {
+        return await Log.find().populate('plantId').exec();
+    }
     
-}
+};
