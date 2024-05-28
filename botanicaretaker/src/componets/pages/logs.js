e766410f8f31779a8c4b593faa40f82657315691
ChelseaBui11 <141456665+ChelseaBui11@users.noreply.github.com>
2024-05-28 10:32:14 -0600

    Logs added to my branch

--- botanicaretaker/src/componets/pages/logs.js
+++ botanicaretaker/src/componets/pages/logs.js
@@ -1,47 +1,50 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './logs.css'
 
 function Logs() {
-
-  const [logs, setLogs] = useState([]);
+  const [logs, setLogs] = useState([])
 
   useEffect(() => {
     const fetchLogs = async () => {
-      const response = await fetch('https://localhost:4206/api/logs');
+      const response = await fetch('http://localhost:4206/api/logs');
       const data = await response.json();
-      setLogs(data);
+      console.log(data)
+      setLogs(data)
     };
     fetchLogs();
   }, []);
 
   return (
-<div className="container">
-      <h1>Logs</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Plant</th>
-            <th>Height</th>
-            <th>Health</th>
-            <th>Stage</th>
+  <div className="container">
+    <h1>Logs</h1>
+    <table>
+      <thead>
+        <tr>
+          <th>Stage</th>
+          <th>Height</th>
+          <th>Health</th>
+          <th>Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* <tr>
+          <th>Test</th>
+          <th>7m</th>
+          <th>Dead</th>
+          <th>2024-05-17</th>
+        </tr> */}
+        {logs ? logs.map((log, index) => (
+          <tr key={index}>
+            <td>{log.stage}</td>
+            <td>{log.height}</td>
+            <td>{log.health}</td>
+            <td>{log.time}</td>
           </tr>
-        </thead>
-        <tbody>
-          {logs.map((log) => (
-            <tr key={log._id}>
-              <td>{new Date(log.time).toLocaleString()}</td>
-              <td>{log.plantId.name}</td>
-              <td>{log.height}</td>
-              <td>{log.health}</td>
-              <td>{log.stage}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-      <button onClick={() => setLogs([])}>Clear Logs</button>
-    </div>
-  );
-}
+        )) : null}
+      </tbody>
+    </table>
+    <button>Clear Logs</button>
+  </div>
+)};
 
-export default Logs;
+export default Logs; 
